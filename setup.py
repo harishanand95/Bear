c2570bdd6792c077e0ac08f8b8a4fe52585cda58
harish <harishanand95@github.com>
2015-12-06 00:41:32 +0530

    changes made to include node.js google speech api

--- setup.py
+++ setup.py
@@ -2,47 +2,38 @@
 # DONT BE ROOT 
 import subprocess
 import platform
-try:
-    import pip
-except:
-    print "pip package not found installing pip..."
+
+def nodejs_install():
     system = platform.linux_distribution()
     if system[0] == 'Fedora':
-        subprocess.call(["sudo", "yum","install","python-pip"])
+        #subprocess.call("curl --silent --location https://rpm.nodesource.com/setup | bash -",shell=True)
+        subprocess.call("sudo yum -y install nodejs gcc-c++ make ",shell=True)
     else:
-        subprocess.call(["sudo", "apt-get","install","python-pip"])
-    import pip
-    pass
-
-
-def install(package):
-    pip.main(['install', package])
+        subprocess.call("curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -",shell=True)
+        subprocess.call("sudo apt-get install -y nodejs",shell=True)
+        subprocess.call("sudo apt-get install -y build-essential",shell=True)
 
 
 if __name__ == '__main__':
     system = platform.linux_distribution()
     if system[0] == 'Fedora':
-        subprocess.call(["sudo", "yum","install","xclip"])
+        subprocess.call(["sudo", "yum","install","xclip","ffmpeg"])
         subprocess.call(["sudo", "yum","install","xdotool"])
-        subprocess.call(["sudo", "yum","install","scipy"])
+        
     else :
-        subprocess.call(["sudo", "apt-get","install","xclip"])
-        subprocess.call(["sudo", "apt-get","install","xdotool","scipy"])
+        subprocess.call(["sudo", "apt-get","install","xclip","ffmpeg"])
+        subprocess.call(["sudo", "apt-get","install","xdotool"])
     
-    install('wikipedia')
+    
+    nodejs_install()
     
     home = subprocess.check_output("echo $HOME", shell=True)
     home = home[:-1]
     cmd = "cp -r ProjectBear@email.com/ " + str(home) + "/.local/share/gnome-shell/extensions/" 
-    
     subprocess.call(["sudo", "chmod","+x","Bear"])
-    subprocess.call(["sudo", "chmod","+x","api_ai"])
-    subprocess.call(["sudo", "chmod","+x","gstt"])
-    subprocess.call(["sudo", "cp","gstt","/usr/local/bin/"])
-    subprocess.call(cmd.split())
-    
+    subprocess.call(cmd.split())    
     permission_cmd = "sudo chmod 757 -R " + str(home) + "/.local/share/gnome-shell/extensions/ProjectBear@email.com/"
-    subprocess.call(permission_cmd.split())
-    subprocess.call(["sudo", "cp","api_ai","/usr/local/bin/"])
+    subprocess.call(permission_cmd.split())    
     subprocess.call(["sudo", "cp","Bear","/usr/local/bin/"])
-    
+    cmd = "cp -r .Bear/ " + str(home) 
+    subprocess.call(cmd.split()) 
