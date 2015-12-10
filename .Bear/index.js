905b192caed4b0f433588e0824e74393b9ea7718
harish <harishanand95@github.com>
2015-12-10 10:56:51 +0530

    Removed Google Speech API Key

--- .Bear/index.js
+++ .Bear/index.js
@@ -1,7 +1,7 @@
 var speech = require('google-speech-api');
 var opts = {
   file:'/tmp/test.wav',
-  key: 'GOOGLE SPEECH API KEY'
+  key: 'ENTER YOUR GOOGLE SPEECH API KEY'
 };
 
 speech(opts, function (err, results) {
